5620c1fe5377911defb139b24a9e578eb22158c7
ShivaniSIngh <shivani.singh12219767@gmail.com>
2022-10-02 16:25:48 +0530

    Third Commit

---
+++ swap.py
@@ -0,0 +1,10 @@
+"""Program to Swap two elements
+Values entered by user"""
+x=int(input("Enter first number: "))
+y=int(input("Enetr second number:"))
+temp=x
+x=y
+y=temp
+#printing swapped values
+print("Now value of first number is:",x)
+print("And value of second number is:",y)
