bb9e66f81bfb667c3ee16d835d66a89e04de6de3
ShivaniSIngh <shivani.singh12219767@gmail.com>
2022-10-08 15:07:08 +0530

    Program

---
+++ Attendance.py
@@ -0,0 +1,7 @@
+attend=int(input("Enter Classes attended by you: "))
+total_classes=int(input("Enter total number of classes: "))
+percent=(attend/total_classes)*100
+if percent>=75:
+    print("Enough Attendance")
+else:
+    print("Less attendance .You need more attendance to give Exam.")
