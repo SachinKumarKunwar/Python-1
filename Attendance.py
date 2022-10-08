ff5cb8319d6ac7aeba0a80a221d035393def8b1f
ShivaniSIngh <shivani.singh12219767@gmail.com>
2022-10-08 15:34:38 +0530

    Program

--- Attendance.py
+++ Attendance.py
@@ -3,5 +3,13 @@ total_classes=int(input("Enter total number of classes: "))
 percent=(attend/total_classes)*100
 if percent>=75:
     print("Enough Attendance")
+    if percent>=90:
+        print("Grade A")
+    elif percent>=80 and percent<90:
+        print("Grade B")
+    elif percent>=75 and percent<80:
+        print("Grade C")
+    elif percent<=75:
+        print("Grade D")
 else:
     print("Less attendance .You need more attendance to give Exam.")
