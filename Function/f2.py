745d61796f6aff5d250948504c79d61552798489
ShivaniSIngh <shivani.singh12219767@gmail.com>
2022-11-05 13:18:13 +0530

    Commit

---
+++ Function/f2.py
@@ -0,0 +1,20 @@
+# Function for Addition 
+
+num1=int(input("Enter first number: "))
+num2=int(input("Enter second number: "))
+
+def add(num1,num2):
+    return num1+num2
+print(add(num1,num2))
+
+
+#Function for Subtraction 
+def sub(num1,num2):
+    return num1-num2
+print(sub(num1,num2))
+
+
+#function for Multiply
+def multiply(num1,num2):
+    return num1*num2
+print(multiply(num1,num2))
