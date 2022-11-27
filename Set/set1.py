7543db42a1bfee9c961be7e2f9e7790a7c698ebb
ShivaniSIngh <shivani.singh12219767@gmail.com>
2022-11-27 23:29:00 +0530

    Commit

---
+++ Set/set1.py
@@ -0,0 +1,12 @@
+set={"car","bike","boat","cycle","car"}
+# print(set)
+# for a in set:
+#     if a=="bike":
+#         print("True")
+#     else:
+#         print("False")
+# # or 
+if "bike" in set:
+    print("True")
+else:
+    print("False")
