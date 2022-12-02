d4f3e6c31c4a4142e26f6387f20e5fc9b0bf9564
ShivaniSIngh <shivani.singh12219767@gmail.com>
2022-12-02 20:35:51 +0530

    Commit

--- List/p1.py
+++
@@ -1,6 +0,0 @@
-#Program to print a new list containing square of every element of previous list.
-number=[1,2,3,4,5]
-newlst=[]
-for a in number:
-    newlst.append(a**2)
-print(newlst)
--- List/p2.py
+++
@@ -1,5 +0,0 @@
-numbers=[1,2,3,4,5,2,6]
-# in the above list find value 2 if it is present replace it with 200 only update the first occurence
-indx=numbers.index(2)
-numbers[indx]=200
-print(numbers)
